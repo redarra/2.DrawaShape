9f37329daff128bcf91b992eb3b2093061f3b516
A252420 <A252420@LSBSA-GQV6HW3>
2024-01-11 12:36:08 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.DrawaShape
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("    / |");
+            Console.WriteLine("   /  |");
+            Console.WriteLine("  /   |");
+            Console.WriteLine("  ____");
+
+            //Freeze Console
+            Console.ReadLine();
+        }
+    }
+}
